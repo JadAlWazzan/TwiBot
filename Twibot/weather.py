f148fac9ee908c0558b8ff9518df83e597d674a2
Jad AL Wazzan <67378553+JadAlWazzan@users.noreply.github.com>
2022-05-10 01:57:48 -0400

    Update weather.py

--- Twibot/weather.py
+++ Twibot/weather.py
@@ -6,7 +6,7 @@ querystring = {"lat":"35.5","lon":"-78.5"}
 
 headers = {
 	"X-RapidAPI-Host": "weatherbit-v1-mashape.p.rapidapi.com",
-	"X-RapidAPI-Key": "c3efd3a460msh47e29fad6939edbp1659edjsn93b4e066eebc"
+	"X-RapidAPI-Key": "**************************************"
 }
 
 response = requests.request("GET", url, headers=headers, params=querystring)
